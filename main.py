734ef6cd6950873f9290b6a4c4419b0507118992
julschoen <julianschoen@gmx.de>
2023-03-03 10:03:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ def main():
 
     transform=transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(0.5, 0.5)
+        transforms.Normalize(
+            (0.5, 0.5, 0.5), 
+            (0.5, 0.5, 0.5))
     ])
 
     dataset1 = datasets.CIFAR10('../data/', train=True, download=True,
